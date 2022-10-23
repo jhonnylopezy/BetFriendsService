45917889330013c8a2708fa60dc804ed97434aa0
Jhonny Lopez <furi110489@hotmail.com>
2022-10-22 20:29:16 -0400

    adicion de nueva clase

---
+++ BetFriends.FE/Models/CrearParticipanteView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BetFriends.FE.Models
+{
+    public class CrearParticipanteView
+    {
+        [Required]
+        [Display(Name = "Usuario")]
+        public string usuario { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Introducir contraseña")]
+        public string clave { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirme su contraseña")]
+        public string claveAConfirmar { get; set; }
+
+
+    }
+}
