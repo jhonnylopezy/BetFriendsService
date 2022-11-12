cd666d884314e4749b991af3866f8917cdf91a0e
Jhonny Lopez <furi110489@hotmail.com>
2022-11-12 12:08:07 -0400

    Implementacion de peticion para registrar pronostico

--- BetFriends.FE/Scripts/global.js
+++ BetFriends.FE/Scripts/global.js
@@ -1,4 +1,7 @@
-﻿var sendDataController = function (action, data, callback, msg) {
+﻿
+var urlService = "http://localhost/betFriendsService/api/";
+
+var sendDataController = function (action, data, callback, msg) {
     console.log("URL: ", document.URL + action + data);
     $.ajax({
 
@@ -8,6 +11,28 @@
         method: "POST",
        // contentType: "application/json",
        // dataType: "json",
+        beforeSend: function () {
+
+        },
+        success: function (result) {
+            callback(result);
+        },
+        error: function (result) {
+            alert(JSON.stringify(result));
+        }
+    });
+}
+
+var sendDataWebService = function (action, data, callback, msg) {
+    //console.log("URL: ",action + data);
+    $.ajax({
+        url: action,
+        crossDomain: true,
+        data: JSON.stringify(data),
+        async: true,
+        method: "POST",
+        contentType: "application/json",
+        dataType: "json",
         beforeSend: function () {
 
         },
